bac889b15c90e8b6b61fa558e602437d6cd0e25d
imKashyap <rahulkashyap4041@gmail.com>
2020-05-16 20:37:04 +0530

    Adding App files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:memory_game/data/data.dart';
+import 'package:memory_game/models/TileModel.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,104 +12,237 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
+  List<TileModel> gridViewTiles = new List<TileModel>();
+  List<TileModel> questionPairs = new List<TileModel>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    reStart();
+  }
+  void reStart() {
+
+    myPairs = getPairs();
+    myPairs.shuffle();
+
+    gridViewTiles = myPairs;
+    Future.delayed(const Duration(seconds: 5), () {
+// Here you can write your code
+      setState(() {
+        print("2 seconds done");
+        // Here you can write your code for open new view
+        questionPairs = getQuestionPairs();
+        gridViewTiles = questionPairs;
+        selected = false;
+      });
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 40,
+              ),
+              points != 800 ? Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "$points/800",
+                    style: TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    "Points",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                        fontSize: 14, fontWeight: FontWeight.w300),
+                  ),
+                ],
+              ) : Container(),
+              SizedBox(
+                height: 20,
+              ),
+              points != 800 ? GridView(
+                shrinkWrap: true,
+                //physics: ClampingScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    mainAxisSpacing: 0.0, maxCrossAxisExtent: 100.0),
+                children: List.generate(gridViewTiles.length, (index) {
+                  return Tile(
+                    imagePathUrl: gridViewTiles[index].getImageAssetPath(),
+                    tileIndex: index,
+                    parent: this,
+                  );
+                }),
+              ) : Container(
+                child: Column(
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          points = 0;
+                          reStart();
+                        });
+                      },
+                      child: Container(
+                        height: 50,
+                        width: 200,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(24),
+                        ),
+                        child: Text("Replay", style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500
+                        ),),
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    GestureDetector(
+                      onTap: (){
+                       // TODO
+                      },
+                      child: Container(
+                        height: 50,
+                        width: 200,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.blue,
+                              width: 2
+                          ),
+                          borderRadius: BorderRadius.circular(24),
+                        ),
+                        child: Text("Rate Us", style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500
+                        ),),
+                      ),
+                    ),
+                  ],
+                )
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+
+class Tile extends StatefulWidget {
+  String imagePathUrl;
+  int tileIndex;
+  _HomeState parent;
+
+  Tile({this.imagePathUrl, this.tileIndex, this.parent});
+
+  @override
+  _TileState createState() => _TileState();
+}
+
+class _TileState extends State<Tile> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (!selected) {
+          setState(() {
+            myPairs[widget.tileIndex].setIsSelected(true);
+          });
+          if (selectedTile != "") {
+            /// testing if the selected tiles are same
+            if (selectedTile == myPairs[widget.tileIndex].getImageAssetPath()) {
+              print("add point");
+              points = points + 100;
+              print(selectedTile + " thishis" + widget.imagePathUrl);
+
+              TileModel tileModel = new TileModel();
+              print(widget.tileIndex);
+              selected = true;
+              Future.delayed(const Duration(seconds: 2), () {
+                tileModel.setImageAssetPath("");
+                myPairs[widget.tileIndex] = tileModel;
+                print(selectedIndex);
+                myPairs[selectedIndex] = tileModel;
+                this.widget.parent.setState(() {});
+                setState(() {
+                  selected = false;
+                });
+                selectedTile = "";
+              });
+            } else {
+              print(selectedTile +
+                  " thishis " +
+                  myPairs[widget.tileIndex].getImageAssetPath());
+              print("wrong choice");
+              print(widget.tileIndex);
+              print(selectedIndex);
+              selected = true;
+              Future.delayed(const Duration(seconds: 2), () {
+                this.widget.parent.setState(() {
+                  myPairs[widget.tileIndex].setIsSelected(false);
+                  myPairs[selectedIndex].setIsSelected(false);
+                });
+                setState(() {
+                  selected = false;
+                });
+              });
+
+              selectedTile = "";
+            }
+          } else {
+            setState(() {
+              selectedTile = myPairs[widget.tileIndex].getImageAssetPath();
+              selectedIndex = widget.tileIndex;
+            });
+
+            print(selectedTile);
+            print(selectedIndex);
+          }
+        }
+      },
+      child: Container(
+        margin: EdgeInsets.all(5),
+        child: myPairs[widget.tileIndex].getImageAssetPath() != ""
+            ? Image.asset(myPairs[widget.tileIndex].getIsSelected()
+                ? myPairs[widget.tileIndex].getImageAssetPath()
+                : widget.imagePathUrl)
+            : Container(
+                color: Colors.white,
+                child: Image.asset("assets/correct.png"),
+              ),
+      ),
     );
   }
 }
